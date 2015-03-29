c89a331cfcf5a8440813013794bd4213aa6b5e96
Robert Hall <langbert@gmail.com>
2015-03-28 22:13:23 -0400

    some config

--- ractive-jade-koa/app.js
+++ ractive-jade-koa/app.js
@@ -1,34 +1,53 @@
-// My SocketStream 0.3 app
 
+// - - - - - - - - - - - - - - - - - - - - - - - - >>>>> vars
+
+// var ssStylus, ractiveOptions = {};
 var http = require('http');
 var ss = require('socketstream');
+var ssJade = require('ss-jade');
 
-// Define a single-page client called 'main'
-ss.client.define('main', {
-	view: 'app.jade',
-	css: ['app.styl'],
-	code: ['libs/jquery.min.js', 'app'],
-	tmpl: '*'
-});
+var config = require('./server/services/config');
+// console.log(config.get('port'));
+// var dbService = require('./server/services/db');
 
-// Serve this client on the root URL
-ss.http.route('/', function(req, res) {
-	res.serveClient('main');
-});
+// - - - - - - - - - - - - - - - - - - - - - - - - >>>>> db services
+
+// dbService.connectMongoose();
+// dbService.connectRedis();
+
+// - - - - - - - - - - - - - - - - - - - - - - - - >>>>> ROUTES
+
+// require('./server/services/routes')(ss);
+
+// // Define a single-page client called 'main'
+// ss.client.define('main', {
+// 	view: 'app.jade',
+// 	css: ['app.styl'],
+// 	code: ['libs/jquery.min.js', 'app'],
+// 	tmpl: '*'
+// });
+
+// // Serve this client on the root URL
+// ss.http.route('/', function(req, res) {
+// 	res.serveClient('main');
+// });
 
 // Code Formatters
 ss.client.formatters.add(require('ss-jade'));
 ss.client.formatters.add(require('ss-stylus'));
 
 // Use server-side compiled Hogan (Mustache) templates. Others engines available
-ss.client.templateEngine.use(require('ss-hogan'));
+// ss.client.templateEngine.use(require('ss-hogan'));
 
 // Minimize and pack assets if you type: SS_ENV=production node app.js
-if (ss.env === 'production') ss.client.packAssets();
+if (ss.env === 'production') {
+	ss.client.packAssets();
+}
 
 // Start web server
 var server = http.Server(ss.http.middleware);
-server.listen(3000);
+server.listen(config.get('port'));
 
 // Start SocketStream
 ss.start(server);
+
---
+++ ractive-jade-koa/server/services/config/development.js
@@ -0,0 +1,8 @@
+
+module.exports = {
+
+	version: '0.0.1',
+	name: 'demo',
+	port: 4444
+
+};
