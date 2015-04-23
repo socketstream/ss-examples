90522bbb419be586db71e9d1e8200c05acd123ac
Robert Hall <langbert@gmail.com>
2015-04-23 00:09:30 -0400

    Cleanup

--- ractive-koa/app.js
+++ ractive-koa/app.js
@@ -11,6 +11,7 @@ var connect = require('koa-connect');
 var session = require('koa-session');
 var router = require('koa-router')();
 var app = koa();
+var server;
 
 // - - - - - - - - - - - - - - - - - - - - - - - - >>>>> config
 
@@ -63,32 +64,6 @@ app.use(router.allowedMethods());
 // Append SocketStream middleware to the stack
 require('./server/services/routes')(ss, app, router);
 app.use(connect(ss.http.middleware));
-// app.use(function*(next) {
-// 	yield ss.http.middleware.bind(null, this.req, this.res);
-// 	yield next;
-// });
-
-// ss.http.middleware.append(app.callback());
-
-// ss.http.middleware.append(function(req, res, next) {
-// 	app.callback.bind(this, req, res);
-// 	next();
-// });
-
-	
-
-	// router.stack.middleware = router.stack.middleware.concat(ss.http.middleware.stack);
-
-	// ss.http.middleware.append(router.middleware());
-
-	// ss.http.middleware.bind(null, app.callback, app.callback);
-
-	// ss.http.middleware.stack.push(app.callback());
-
-	// app.stack = app.stack.concat(ss.http.middleware.stack)
-
-	// ss.http.middleware.stack = 
-
 
 // - - - - - - - - - - - - - - - - - - - - - - - - >>>>> PACK ASSETS
 
@@ -97,17 +72,25 @@ if (ss.env === 'production') {
 	ss.client.packAssets();
 }
 
-ss.start(http.createServer(app.callback()).listen(config.get('port')));
-// ss.start(http.createServer(ss.http.middleware).listen(config.get('port')));
-
-// var server = app.listen(config.get('port'), function() {
-// 	process.on('uncaughtException', function(err) { console.log(err); });
-// 	var local = server.address();
-// 	console.log('Koa server listening @ http://%s:%d/ in %s mode', local.address, local.port, app.env);
-// 	// Start SocketStream
+// server = http.createServer(app.callback()).listen(config.get('port'), function() {
+// 	console.log(server.address());
 // 	ss.start(server);
-// 	console.log('Ready...set...SubAtomic!'.green);
 // });
+
+
+server = app.listen(config.get('port'), function() {
+	var local = server.address();
+
+	console.log('Koa server listening @ http://%s:%d/ in %s mode', local.address, local.port, app.env);
+
+	// Start SocketStream
+	ss.start(server);
+
+	process.on('uncaughtException', function(err) {
+		console.log(err);
+	});
+
+});
 
 
 
