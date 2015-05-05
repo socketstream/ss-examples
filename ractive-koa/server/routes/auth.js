df9670a4b9abc6843c5f11e8be9a6b5562da09ed
Robert Hall <langbert@gmail.com>
2015-05-05 08:46:08 -0400

    Yields instead of callbacks.

--- ractive-koa/server/routes/auth.js
+++ ractive-koa/server/routes/auth.js
@@ -6,31 +6,41 @@ module.exports = function(ss, app, router) {
 
 	// - - - - - - - - - - - - - - - - - - - - - - - - >>>>> Pages
 
-	router.get('/signin', function*(next) {
+	router.get('/signin', function*() {
 		this.render('auth/signin', {}, true);
 	});
 
-	router.get('/signout', function*(next) {
+	router.get('/signout', function*() {
 		this.render('auth/signout', {}, true);
 	});
 
 	// - - - - - - - - - - - - - - - - - - - - - - - - >>>>> API
 
-	router.post('/api/join', function*(next) {
+	router.post('/api/join', function*() {
 		this.body = this.request.body;
 	});
 
-	router.post('/api/signin', function*(next) {
-		var newUser = new User(this.request.body);
-		newUser.save(function(err, user) {
-			if (err) {
-				return console.error(err);
-			}
-		});
-		this.body = this.request.body;
+	router.post('/api/signin', function*() {
+		var body = this.request.body;
+		var user = yield User.findOne({ username: body.username });
+		if (user) {
+			console.log('Username already taken'.red.inverse); // TODO: username or email
+			this.body = 'Username already taken';
+		} else {
+			console.log('no user found!'.green.inverse, user);
+			var newUser = new User({
+				username: body.username,
+				password: body.password,
+				email: body.email
+			});
+			user = yield newUser.save();
+			console.log(user);
+			console.log('New user created'.green.inverse);
+			this.body = user;
+		}
 	});
 
-	router.get('/api/signout', function*(next) {
+	router.get('/api/signout', function*() {
 	});
 
 };
