e86aca9af83b2d3a7c6edb8612a2962714e5da31
Robert Hall <langbert@gmail.com>
2015-05-17 17:26:08 -0400

    Replicating session store in local instance...
    ...to do something with it w/ Koa.

--- ractive-koa/server/db/redis.js
+++ ractive-koa/server/db/redis.js
@@ -1,6 +1,9 @@
 'use strict';
 
 var config = require('../config');
+var session = require('express-session');
+var RedisStore = require('connect-redis')(session);
+var redisStore;
 
 module.exports = {
 
@@ -8,11 +11,13 @@ module.exports = {
 
 		var redisConfig = config.get('redis');
 
-		console.log('REDIS connected...'.green);
-
-		ss.session.store.use('redis', redisConfig);
+		// ss.session.store.use('redis', redisConfig);
+		redisStore = new RedisStore(redisConfig);
+		ss.session.store.use(redisStore);
 		ss.publish.transport.use('redis', redisConfig);
 		ss.session.options.maxAge = 1000 * 60 * 60 * 24 * 30; // one month
+
+		console.log('REDIS connected...'.green);
 
 	}
 
