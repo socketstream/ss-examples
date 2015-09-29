40bff4a4d2bdfadb2e468dde5e1958bdb466a713
Henrik Vendelbo <henrik@thepia.com>
2015-09-29 20:11:29 +0200

    building an express example

---
+++ express-4-and-js/app.js
@@ -0,0 +1,30 @@
+// My SocketStream 0.5 app
+
+var ss = require('socketstream');
+
+// Define a single-page client called 'main'
+ss.client.define('main', {
+  view: 'app.jade',
+  css:  ['../node_modules/normalize.css/normalize.css', 'app.scss'],
+  code: ['../node_modules/es6-shim/es6-shim.js', 'libs/jquery.min.js', 'app'],
+  tmpl: '*'
+});
+
+// Serve this client on the root URL
+ss.http.route('/', function(req, res){
+  res.serveClient('main');
+});
+
+ss.client.formatters.add('sass');
+ss.client.formatters.add('jade');
+
+// Use server-side compiled Hogan (Mustache) templates. Others engines available
+ss.client.templateEngine.use(require('ss-hogan'));
+
+// Minimize and pack assets if you type: SS_ENV=production node app.js
+if (ss.env === 'production') ss.client.packAssets();
+
+ss.ws.transport.use('sockjs');
+
+// Start SocketStream
+ss.start();
